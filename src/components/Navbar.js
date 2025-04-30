c9f4e315dea2ce54520e56388d2f009e54a7a99e
msmsahu <msmsahu191@gmail.com>
2025-04-30 15:02:52 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <Link className="navbar-brand" to="">{props.title}</Link>
     
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/about">About</Link>
